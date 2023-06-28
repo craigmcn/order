ac1bf482ed911d642ade5898ff7ae698bf5566fb
Craig McNaughton <therealcraigmcn@gmail.com>
2023-06-27 19:05:34 -0700

    Update for Netlify deploy
    - add subdirectory to config
    - add .eslintignore
    - update package name, license, repo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import eslint from 'vite-plugin-eslint';
 
 export default {
+  base: './',
+  build: {
+    rollupOptions: {
+      output: [
+        { dir: 'dist' },
+        { dir: 'dist/order' }, // For Netlify subdirectory
+      ],
+    },
+  },
   server: {
     port: 5510,
   },
