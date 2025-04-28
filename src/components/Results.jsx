fc4286ebe3f285863cd66de4cd921bb2fb4847e3
Craig McNaughton <therealcraigmcn@gmail.com>
2025-04-28 12:15:26 -0700

    Copy button should provide feedback when clicked
    - move CopyButton to separate component
    - update `handleCopy` function with state
    - update button classes for click state

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,9 @@
 import { useContext } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-84f13ff524/icons';
-import { copyToClipboard, generate } from '../utils';
-import { NamesContext } from '../contexts/NamesContext';
-import { DEFAULT_OPTIONS } from '../utils/constants';
 import { useLocalStorage } from '@uidotdev/usehooks';
+import { generate } from '../utils';
+import { DEFAULT_OPTIONS } from '../utils/constants';
+import { NamesContext } from '../contexts/NamesContext';
+import CopyButton from './CopyButton';
 
 function Results() {
   const { currentNames } = useContext(NamesContext);
@@ -12,25 +11,12 @@ function Results() {
   const localOptions = storedOptions ? { ...DEFAULT_OPTIONS, ...storedOptions } : DEFAULT_OPTIONS;
 
   const results = generate(currentNames?.names, localOptions);
-
-  const handleCopy = () => {
-    copyToClipboard(results);
-  };
-
   return (
     <div
       className={`group relative mx-auto my-8 py-3 px-5 text-2xl text-slate-700 dark:text-slate-300 border rounded border-slate-300 dark:border-slate-700 focus:border-slate-400 dark:focus:border-slate-600 outline-none ${results ?? 'hidden'}`}
       tabIndex={0} // eslint-disable-line jsx-a11y/no-noninteractive-tabindex
     >
-      <button
-        className="absolute top-0 right-0 z-10 mt-1 mr-1 p-1 bg-white dark:bg-slate-900 bg-opacity-75 dark:bg-opacity-50 backdrop-blur-sm text-slate-600 dark:text-slate-400 rounded-sm invisible group-hover:visible group-focus-within:visible outline-none outline-offset-1 focus:outline-orange-500/50"
-        type="button"
-        title="Copy"
-        onClick={handleCopy}
-      >
-        <FontAwesomeIcon data-testid="copy-button-icon" icon={byPrefixAndName.fal['copy']} />
-      </button>
-
+      <CopyButton text={results} />
       <div data-testid="results">{results}</div>
     </div>
   );
